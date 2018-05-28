3d910fc77917891f6b7df7d1b8706438e8e2ddf1
Babubhai <shubhsharma10@gmail.com>
2018-05-28 11:22:48 -0400

    Topic card styling and redirection bug fixed

--- src/components/TopicCard.js
+++ src/components/TopicCard.js
@@ -6,14 +6,16 @@ export default class TopicCard extends React.Component {
     render() {
         return(
             <div className="card">
-               <Link to={`/course/${this.props.courseId}/module/${this.props.moduleId}/lesson/${this.props.lessonId}/topic/${this.props.topic.id}`}
-                     >
-                   <h5 className="card-header">{this.props.topic.title}</h5>
-                   <button className="btn btn-danger float-right btn-sm" onClick={() =>
-                   {this.props.delete(this.props.topic.id,this.props.topic.title)}}>
-                       <i className="fa fa-times"/>
-                   </button>
-               </Link>
+                <div className="card-header">
+                    <Link to={`/course/${this.props.courseId}/module/${this.props.moduleId}/lesson/${this.props.lessonId}/topic/${this.props.topic.id}`}
+                    >
+                        <h5 >{this.props.topic.title}</h5>
+                    </Link>
+                </div>
+                <button className="btn btn-danger float-right btn-sm" onClick={() =>
+               {this.props.delete(this.props.topic.id,this.props.topic.title)}}>
+                   <i className="fa fa-times"/>
+               </button>
             </div>
         );
     }
